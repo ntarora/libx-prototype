4fedf1a9821858ac7a9cc6ff20d6edae7a6f89d7
Neel Arora <ntarora@vt.edu>
2017-04-03 20:16:15 -0400

    tesing libx intergration for ISBNs

--- toolTip.js
+++ toolTip.js
@@ -6,77 +6,85 @@ require("./tooltipster-sideTip-light.min.css");
 var tooltipster = require("./tooltipster.bundle.min")($);
 
 
+
+
 /**
  * adds tool tip inside of the node
  * @param nodeName - id of the node adding the tool tip
  */
 export default function addToolTip(nodeName) {
 
-    // var $lookup = $("&lt;p&gt;Searching for " + "ISBN" + " in OCLC xISBN's database...&lt;/p&gt;");
+
     // var $xisbnResult = $("&lt;p style='padding-top:5px;'&gt;");
+    let isbn = $(`#${nodeName}`).text()
+    var $lookup = $(`<p>Searching for ${isbn} in OCLC xISBN's database...</p>`)
+    var $xisbnResult = $("<p style='padding-top:5px'>");
 
     $(`#${nodeName}`).tooltipster({
-        content: [`${$lookup.html()}`, `${$xisbnResult.html()}`],
+        content: [$lookup, $xisbnResult],
         theme: 'tooltipster-light',
         trigger: 'hover',
-        interactive: true
-
+        interactive: true,
+        contentAsHTML: true
     })
 
-
 }
-// function getToolTipConent(isbn) {
-//     var cat = libx.edition.catalogs.primary;
-//     var url;
-//     var lookupString;
-//     var xisbnResultString =
-//     if (cat.supportsSearchType('i')) {
-//         // this method handles xISBN if configured
-//         url = cat.linkByISBN(isbn);
-//     } else if (cat.supportsSearchType('Y')) {
-//         url = cat.makeKeywordSearch(isbn);
-//     } else {
-//         return;
-//     }
-//     libx.services.xisbn.getISBNMetadata({
-//         isbn: isbn,
-//         ifFound: function (text, metadata) {
-//             $lookup.text(tuple.isbn + " refers to: " + text);
-//             askForILL.note({metadata: metadata});
-//         },
-//         notFound: function () {
-//             $lookup.html("This ISBN is not known to OCLC. No metadata is available.");
-//         }
-//     });
-//
-//     if (('summonproxyurl' in cat) || (cat.url.indexOf('summon.serialssolutions.com') &gt; 0)) {
-//         libx.services.xisbn.getISBNEditions({
-//             isbn: tuple.isbn,
-//             ifFound: function (editions) {
-//
-//                 var nEditions = editions.split(",").length;
-//                 $xisbnResult.html(nEditions + " editions of this item have been published.");
-//
-//                 var $summon = $("&lt;p&gt;").text("Checking holdings of all editions in " + cat.name + "...");
-//                 $xisbnResult.after($summon);
-//
-//                 var searchInputs = [];
-//                 var searchInput = {};
-//                 searchInput.searchTerms = editions.replace(/,/g, " ");
-//                 searchInput.searchType = 'i';
-//
-//                 searchInputs.push(searchInput);
-//                 var output = {
-//                     searchparams: searchInputs,
-//                     placeholder: $summon
-//                 };
-//
-//                 libx.space.write(output);
-//                 libx.space.write({issearchlibapp: false});
-//
-//             }
-//         });
-//     }
-// }
+
+function getToolTipConent(isbn) {
+    var cat = libx.edition.catalogs.primary;
+    var url;
+    var lookupString;
+    var xisbnResultString;
+    if (cat.supportsSearchType('i')) {
+        // this method handles xISBN if configured
+        url = cat.linkByISBN(isbn);
+    } else if (cat.supportsSearchType('Y')) {
+        url = cat.makeKeywordSearch(isbn);
+    } else {
+        return;
+    }
+    libx.services.xisbn.getISBNMetadata({
+        isbn: isbn,
+        ifFound: function (text, metadata) {
+            //write to the tool to tip when found
+            //$lookup.text(isbn + " refers to: " + text);
+            //askForILL.note({metadata: metadata});
+        },
+        notFound: function () {
+               // write to the tooltip not found
+            //$lookup.html("This ISBN is not known to OCLC. No metadata is available.");
+        }
+    });
+
+    if (('summonproxyurl' in cat) || (cat.url.indexOf('summon.serialssolutions.com'))) {
+        libx.services.xisbn.getISBNEditions({
+            isbn: isbn,
+            ifFound: function (editions) {
+
+                var nEditions = editions.split(",").length;
+                //write after editions have been found in summon write to the tool tip
+                //$xisbnResult.html(nEditions + " editions of this item have been published.");
+
+                //var $summon = $("&lt;p&gt;").text("Checking holdings of all editions in " + cat.name + "...");
+                //$xisbnResult.after($summon);
+
+                // var searchInputs = [];
+                // var searchInput = {};
+                // searchInput.searchTerms = editions.replace(/,/g, " ");
+                // searchInput.searchType = 'i';
+                //
+                // searchInputs.push(searchInput);
+                // var output = {
+                //     searchparams: searchInputs,
+                //     placeholder: $summon
+                // };
+                //
+                // libx.space.write(output);
+                // libx.space.write({issearchlibapp: false});
+
+            }
+        });
+    }
+}
 
 
